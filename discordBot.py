ab16a0bb832033730fb6a49e81d3b5df66f1f0fc
StarklyUnnamed <sshind90192@gmail.com>
2023-04-18 17:19:27 -0500

    Added 'on_message' event handler. Added 'on_error' event handler. Added 'err.log' to .gitignore.

--- discordBot.py
+++ discordBot.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import discord
 from dotenv import load_dotenv
@@ -20,8 +21,8 @@ async def on_ready():
         f"{guild.name}(id: {guild.id})"
     )
     
-    members = '\n +'.join(member.name for member in guild.members)
-    print(f"The members of the guild are \n +{members}")
+    members = '\n -'.join(member.name for member in guild.members)
+    print(f"The members of the guild are \n -{members}\n")
 
 @client.event
 async def on_member_join(member):
@@ -29,5 +30,39 @@ async def on_member_join(member):
     await member.dm_channel.send(
         f"Hello {member.name}. Welcome to Bot Development."
     )
+    print(f"-{member.name} has been welcomed to the Guild.")
+    
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if str(client.user.id) in message.content:
+        randomResponses = [
+            "He lost his ticket for the movie.",
+            "I can hardly wait till I see you.",
+            "My father loves pizza.",
+            "I don't know what Mary is looking for.",
+            "He's in bed with the flu.",
+            "I'll pay the bill.",
+            "You can say whatever you want to.",
+            "OK. Now what?",
+            "What time did you wake up this morning?",
+            "The bottle smashed to pieces.",
+            "Well, you could go down to 22nd street. There are lot of stores down there that are open 24 hours a day.",
+            "He was dropped from the team for using drugs."
+        ]
+        response = random.choice(randomResponses)
+        await message.channel.send(response)
+        print(f"Responded to {message.author}")
+    elif message.content == 'raise-exception':
+        raise discord.DiscordException
+
+@client.event
+async def on_error(error, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if error == 'on_message':
+            f.write(f"Unhandled Message: {args[0]}\n")
+            print('Logged Error... \n')
 
 client.run(TOKEN)
