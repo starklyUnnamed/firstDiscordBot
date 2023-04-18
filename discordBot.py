739d4ec8c70d1bd6f2a7dc12ea1bcfe9fb9ae4c6
StarklyUnnamed <sshind90192@gmail.com>
2023-04-18 16:05:43 -0500

    Classes proved difficult to implement. Moving on...

--- discordBot.py
+++ discordBot.py
@@ -10,8 +10,24 @@ TOKEN = os.getenv('discordToken')
 guildCorrect = os.getenv('discordGuild')
 
 
-class ConnectToDiscord():
-    async def on_ready(self):
-        print(f"{self.user} has connected to Discord!")
+client = discord.Client(intents=intents)
 
-ConnectToDiscord().run(TOKEN)
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=guildCorrect)
+    print(
+        f"{client.user} has connected to the following Guild: \n"
+        f"{guild.name}(id: {guild.id})"
+    )
+    
+    members = '\n +'.join(member.name for member in guild.members)
+    print(f"The members of the guild are \n +{members}")
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f"Hello {member.name}. Welcome to Bot Development."
+    )
+
+client.run(TOKEN)
