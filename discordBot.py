e887413f62d13d3f41258133ad22aebecc1da677
StarklyUnnamed <sshind90192@gmail.com>
2023-04-18 14:59:03 -0500

    'discordBot.py' now connects to the 'Bot Development' guild and lists all members.

--- discordBot.py
+++ discordBot.py
@@ -1,3 +1,25 @@
 import os
 
 import discord
+from dotenv import load_dotenv
+
+intents = discord.Intents.default().all()
+
+load_dotenv()
+TOKEN = os.getenv('discordToken')
+guildCorrect = os.getenv('discordGuild')
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=guildCorrect)
+    print(
+        f"{client.user} has connected to the following Guild: \n"
+        f"{guild.name}(id: {guild.id})"
+    )
+    
+    members = '\n +'.join(member.name for member in guild.members)
+    print(f"The members of the guild are \n +{members}")
+
+client.run(TOKEN)
